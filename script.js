dccb3a3e9cff368ee7b63340c046b253c4ca9ea3
Matt McKeever <mmckeever23@gmail.com>
2022-12-12 21:53:51 -0600

    Added pin and info window

--- script.js
+++ script.js
@@ -8,6 +8,45 @@ function initMap() {
   const marker = new google.maps.Marker({
     position: {lat: 38.62727, lng: -90.19789},
     map: map,
+    icon: "https://img.icons8.com/tiny-color/32/null/map-pin.png",
     draggable: true
+  });
+
+  const content = 
+  "<h2>Create New Pin</h2>" +
+    "<form>" +
+      "<label>Location: </label>" +
+      "<input type='text' required><br>" +
+      "<br>" +
+      "<label>Date Arrived: </label>" +
+      "<input type='date' required><br>" +
+      "<br>" +
+      "<label>Date Departed: </label>" +
+      "<input type='date' required><br>" +
+      "<br>" +
+      "<label>Companions: </label><br>" +
+      "<input type='checkbox'>" +
+      "<label>Mom</label><br>" +
+      "<input type='checkbox'>" +
+      "<label>Dad</label><br>" +
+      "<input type='checkbox'>" +
+      "<label>Julie</label><br>" +
+      "<p style='margin-left: 13px'>+ Add Companion</p>" +
+      "<label>Details: </label><br>" +
+      "<textarea placeholder='What did you do there?' rows='10' cols='29'></textarea><br>" +
+      "<br>" +
+      "<input type='submit' value='Create Pin'>" +
+      "<br>" +
+    "</form>"
+
+  const detailWindow = new google.maps.InfoWindow({
+    content: content,
+  })
+
+  marker.addListener("click", function() {
+    detailWindow.open({
+      anchor: marker,
+      map,
+    });
   });
 }
